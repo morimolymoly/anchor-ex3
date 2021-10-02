27a426bc289b3f17b74f760aa4dbe48354245f43
morimolymoly <morimolymoly@gmail.com>
2021-10-02 16:41:37 +0900

    impl

--- tests/ex3.js
+++ tests/ex3.js
@@ -1,14 +1,44 @@
+const assert = require('assert');
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 describe('ex3', () => {
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.local();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Ex3;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  const counter = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Ex3;
+
+  it('create a counter', async () => {
+    await program.rpc.create(provider.wallet.publicKey, {
+      accounts: {
+        counter: counter.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [counter],
+    });
+
+    let counterAcc = await program.account.counter.fetch(counter.publicKey);
+
+    assert.ok(counterAcc.authority.equals(provider.wallet.publicKey));
+    assert.ok(counterAcc.count.toNumber() ==  0);
+  });
+
+  it('update a counter', async () => {
+    await program.rpc.increment({
+      accounts: {
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    })
+
+    const counterAcc = await program.account.counter.fetch(counter.publicKey);
+
+    assert.ok(counterAcc.authority.equals(provider.wallet.publicKey))
+    assert.ok(counterAcc.count.toNumber() == 1)
   });
 });
